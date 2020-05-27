2b56a0f1512c313fcef437ffdfb4ac3d587a9ac3
maheshg23 <gmn1994@gmail.com>
2020-05-27 17:27:29 -0400

    Add React Redux Auth state

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,22 +1,22 @@
-const config = require('config');
-const jwt = require('jsonwebtoken');
+const config = require("config");
+const jwt = require("jsonwebtoken");
 
-function auth(req, res, next){
-    const token = req.header('x-auth-token');
+function auth(req, res, next) {
+  const token = req.header("x-auth-token");
 
-    //check for token
-    if(!token) res.status(401).json({ msg: 'No token, authorization denied '});
-    
-    try{
-        //Verify the token
-        const decoded = jwt.verify(token, config.get('jwtSecret'));
-        //Add user from payload 
-        req.user = decoded;
-        next();
-    }catch(e){
-        res.status(400).json({ msg: 'TOken is not valid'});
-    }
-    
+  //check for token
+  if (!token)
+    return res.status(401).json({ msg: "No token, authorization denied " });
+
+  try {
+    //Verify the token
+    const decoded = jwt.verify(token, config.get("jwtSecret"));
+    //Add user from payload
+    req.user = decoded;
+    next();
+  } catch (e) {
+    res.status(400).json({ msg: "TOken is not valid" });
+  }
 }
 
 module.exports = auth;
