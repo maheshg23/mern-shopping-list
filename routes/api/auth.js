0cbbecfcba54203e519a0f7f7756f32a6584e854
maheshg23 <gmn1994@gmail.com>
2020-05-28 18:14:24 -0400

    Add React Registration

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,56 +1,98 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const bcrypt = require('bcryptjs');
-const config = require('config');
-const jwt = require('jsonwebtoken');
-const auth = require('../../middleware/auth');
+const bcrypt = require("bcryptjs");
+const config = require("config");
+const jwt = require("jsonwebtoken");
+const auth = require("../../middleware/auth");
 
-const Item = require('../../models/User')
+const JWT_SECRET = config.get("jwtSecret");
+
+console.log;
+const Item = require("../../models/User");
 
 //* @route POST api/auth
-router.post('/',(req, res) => {
-   const { email, password } = req.body;
+router.post("/register", async (req, res) => {
+  const { name, email, password } = req.body;
 
-   if(!email || !password){
-       return res.status(400).json({ msg: 'Please enter all the details'})
-   }
+  // Simple validation
+  if (!name || !email || !password) {
+    return res.status(400).json({ msg: "Please enter all fields" });
+  }
 
-   //Check existing user 
-   User.findOne({ email })
-    .then(user => {
-        if(!user) return res.status(400).json({ msg: 'User does not exists'})
+  try {
+    const user = await User.findOne({ email });
+    if (user) throw Error("User already exists");
 
-        //Compare the password with the hash value in the database
-        bcrypt.compare(password, user.password)
-            .then(isMatch => {
-                if(!isMatch) return res.status(400).json({ msg: 'Invalid Credentials'});
+    const salt = await bcrypt.genSalt(10);
+    if (!salt) throw Error("Something went wrong with bcrypt");
 
-                jwt.sign(
-                    { id: user.id },
-                    config.get('jwtSecret'),
-                    { expiresIn: 3600 },
-                    (err, token) => {
-                        if(err) throw err;
-                        res.json({
-                            token,
-                            user: {
-                                id: user.id,
-                                name: user.name,
-                                email: user.email
-                            }
-                        });
-                    }
-                )
-            })
-        
-    })
+    const hash = await bcrypt.hash(password, salt);
+    if (!hash) throw Error("Something went wrong hashing the password");
+
+    const newUser = new User({
+      name,
+      email,
+      password: hash,
+    });
+
+    const savedUser = await newUser.save();
+    if (!savedUser) throw Error("Something went wrong saving the user");
+
+    const token = jwt.sign({ id: savedUser._id }, JWT_SECRET, {
+      expiresIn: 3600,
+    });
+
+    res.status(200).json({
+      token,
+      user: {
+        id: savedUser.id,
+        name: savedUser.name,
+        email: savedUser.email,
+      },
+    });
+  } catch (e) {
+    res.status(400).json({ error: e.message });
+  }
 });
 
+// //* @route POST api/login
+// router.post("/login", async (req, res) => {
+//   const { email, password } = req.body;
+
+//   // Simple validation
+//   if (!email || !password) {
+//     return res.status(400).json({ msg: "Please enter all fields" });
+//   }
+
+//   try {
+//     // Check for existing user
+//     const user = await User.findOne({ email });
+//     if (!user) throw Error("User Does not exist");
+
+//     const isMatch = await bcrypt.compare(password, user.password);
+//     if (!isMatch) throw Error("Invalid credentials");
+
+//     const token = jwt.sign({ id: user._id }, JWT_SECRET, { expiresIn: 3600 });
+//     if (!token) throw Error("Couldnt sign the token");
+
+//     res.status(200).json({
+//       token,
+//       user: {
+//         id: user._id,
+//         name: user.name,
+//         email: user.email,
+//       },
+//     });
+//   } catch (e) {
+//     res.status(400).json({ msg: e.message });
+//   }
+// });
+
 //* @route GET api/auth/user
-router.get('/user', auth, (req,res) => {
-    User.findById(req.user.id)
-        .select('-password')
-        .then(user => res.json(user));
-})
+router.get("/user", auth, (req, res) => {
+  User.findById(req.user.id)
+    .select("-password")
+    .then((user) => res.json(user));
+});
 
 module.exports = router;
